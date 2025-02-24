1fdd1297b3c71b0ea870b3c4b0057fb3d48a4061
mutuamusembi <musembif583@gmail.com>
2025-02-24 15:52:53 +0530

    Add files via upload

---
+++ simple_interest.C.c
@@ -0,0 +1,23 @@
+//program to calculate simple interest
+// Felix musembi
+/*
+ADM NUMBER:
+D33-2395-2023
+*/
+#include <stdio.h>
+int main()
+{
+  int principle;
+  int rate;
+  int time;
+  int simple_interest;
+  printf("principle:\n");
+  scanf("%d",&principle);
+  printf("rate:\n");
+  scanf("%d",&rate);
+  printf("time:\n");
+  scanf("%d",&time);
+  simple_interest=principle*rate*time/100;
+  printf("simple interest:%d\n", simple_interest);
+  return 0;
+}
